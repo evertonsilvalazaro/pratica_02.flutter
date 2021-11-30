87d4162faa02742e22dcadfa108dc1a5cf4b5c8a
Everton Silva lasaro <everton.pratika@gmail.com>
2021-11-30 20:12:39 -0300

    subindoo pratica 2, ceverino e wesley

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    Center(
+      child: Text(
+        'Vitor Gabriel Gonçalves Ferreira - RA: 42015305',
+        textDirection: TextDirection.ltr,
+      ),
+    ),
+  );
+}
